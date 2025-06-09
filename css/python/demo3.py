9b5f899abe14875077eafeb9ed3ac882fdee5723
Rupesh <rupeshsenthilkumar@gmail.com>
2025-06-09 15:57:59 +0530

    html

---
+++ css/python/demo3.py
@@ -0,0 +1,5 @@
+dictionary={"name":"vicky","password":"1234"}
+print(dictionary["name"])
+print(type(dictionary))
+for key in dictionary:
+    print(dictionary[key])
---
+++ jawascript/python/demo.py
@@ -0,0 +1,10 @@
+a =1
+b =2
+print(a+b)
+print(type(a))
+c ="string"
+d = true
+pi =3.14
+print(type(c))
+print(type(d))
+print(type(pi))
